7219c47e0ebf8827b5f26079703ff7b24796c334
Mika Respati <74171055+mikarespati@users.noreply.github.com>
2020-11-16 22:35:55 +0700

    Dummy

---
+++ landing_page/src/components/LandingPage.vue
@@ -0,0 +1,338 @@
+<template>
+    <div class="container-fluid m-0 p-0">
+      <!--<div id="header" class="full-height p-4">
+          <div class="container text-light">
+              <div class="container">
+                  <ul id="nav" class="nav justify-content-center m-0 p-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Active</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                    </ul>
+              </div>
+
+              <div class="row pb-5">
+
+                  <div class="col-6">
+                      <p>Text</p>
+                  </div>
+                  
+                  <div class="col-6">
+                      <p>Gambar</p>
+                  </div>
+
+              </div>
+          </div>
+      </div>
+      <div id="product" class="bg-primary p-4">
+
+      </div>
+      <div id="testimonial" class="bg-secondary p-4">
+
+      </div>
+      -->
+      <div id="why-ca" class="text-light p-4">
+          <div class="container">
+              <div class="row">
+                  <div class="col">
+                    <img src="../assets/Magang/why-ca/Continious Improvment.png" class="pt-4">
+                  </div>
+                  <div class="col">
+                    <img src="../assets/Magang/why-ca/Rich Result.png" class="pt-4">
+                  </div>
+                  <div class="col">
+                    <img src="../assets/Magang/why-ca/Continious Improvment-1.png" class="pt-4">
+                  </div>
+                  <div class="col">
+                    <img src="../assets/Magang/why-ca/Reducing The Risk of Vulnarebility.png" class="pt-4">
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div id="security-testing" class="p-4 p-md-5">
+              <div id="sec" class="container content-center">
+                <div id="text" class="text-center">
+                <h1 id="pro-text">SECURITY TEXT</h1>
+                <p id="pr-text">Can be done on the following types of platform</p>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div id="card" class="border rounded col-lg m-1">
+                            <img src="../assets/Magang/icon-web-app-5.png" height="120" class="m-4">
+                            <p id="card-text">Web Application</p>
+                        </div>
+                        <div class="border rounded col-lg m-1">
+                            <img src="../assets/Magang/icon-android-app-5.png" height="120" class="m-4">
+                            <p id="card-text">Android</p>
+                        </div>
+                        <div class="border rounded col-lg m-1">
+                            <img src="../assets/Magang/icon-ios-app-5.png" height="120" class="m-4">
+                            <p id="card-text">IOS</p>
+                        </div>
+                        <div class="border rounded col-lg m-1">
+                            <img src="../assets/Magang/icon-api-app-1.png" height="120" class="m-4">
+                            <p id="card-text">API</p>
+                        </div>
+                    </div>
+                </div>
+              </div>
+      </div>
+      <div id="counter" class="p-4">
+          <div class="container-fluid">   
+                    <div class="row text-light">
+                        <div id="pos1" class="col">
+                            <h1 id="cc-text">303</h1>
+                            <p id="cc-text-2">Verifed BugHunters from<br>the Community and Professionals</p>
+                        </div>
+                        <div id="pos2" class="col">
+                            <h1 id="cc-text">< 404</h1>
+                            <p id="cc-text-2">The fastest Critical<br> Vulnerability Finding received</p>
+                        </div>
+                        <div id="pos3" class="col">
+                            <h1 id="cc-text">22 Minutes</h1>
+                            <p id="cc-text-2">The fastest High<br> Vulnerability Finding received</p>
+                        </div>
+                        <div id="pos4" class="col">
+                            <h1 id="cc-text">1006</h1>
+                            <p id="cc-text-2">Vulnerability Reports<br> From Low to Critical</p>
+                        </div>
+                        <div id="pos5" class="col">
+                            <h1 id="cc-text">100+ </h1>
+                            <p id="cc-text-2">Applications from<br> various industrial sectors</p>
+                        </div>
+                    </div>
+          </div>
+      </div>
+      <div id="portofolio" class="p-4">
+          <div id="content-por" class="container">
+              <h1 id="por-text">OUR BUGHUNTERS ACKNOWLEDGED BY</h1>
+              <br>
+              <img src="../assets/Magang/logo.png" alt="">
+              <br><br><br>
+              <button type="button" class="btn btn-outline-primary">Show More</button>
+          </div>
+      </div>
+      <div id="partnership" class="p-4">
+          <div class="container">
+              <p id="por-text">PARTNERSHIP</p>
+              <img src="../assets/Magang/logo2.png" alt="">
+          </div>
+      </div>
+      <div id="footer" class="p-4">
+          <div class="container">
+               <div class="row">
+                    <div class="col text-left">
+                        <p id="footer-text">CyberArmy Indonesia</p>
+                        <p id="footer-text2">
+                            About<br><br>
+                            Publicatioan<br><br>
+                            Partnership<br><br>
+                            Contact<br><br>
+                            Carrier<br><br>
+                        </p>
+                    </div>
+                    <div class="col text-left">
+                        <p id="footer-text">Company Services</p>
+                        <p id="footer-text2">
+                            Vulnerability Management Program <br><br>
+                            Bug Bounty Program <br><br>
+                            CyberArmy Academy <br><br>
+                        </p>
+                    </div> 
+                    <div class="col text-left">
+                        <p id="footer-text">BUGHUNTER AREA</p>
+                        <p id="footer-text2">
+                            Challenge Program<br><br>
+                            Program List <br><br>
+                            Leaderboard <br> <br>
+                        </p>
+                    </div> 
+                    <div class="col text-left">
+                        <p id="footer-text">CyberArmy Indonesia</p>
+                        <p id="footer-text2">
+                            OTHERS <br><br>
+                            FAQ <br><br>
+                            Term & Condition <br><br>
+                            Privacy Policy
+                        </p>
+                    </div> 
+                    <div class="col">
+                        <p id="footer-text">Follow Us</p>
+                        <img src="../assets/Magang/Group 267.png" alt="">
+                    </div>        
+                </div>
+          </div>
+          <p id="copy">© 2020 PT Global Inovasi Siber Indonesia - All Rights Reserved</p>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    #header{
+        background: url(../assets/Magang/Rectangle_background.png) no-repeat center center;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
+    #why-ca{
+        background-color: #252E71;
+        height: 267px;
+        left: 0px;
+        top: 2046px;
+    }
+    #security-testing{
+        height: 525px;
+        width: 1200px; 
+        
+    }
+    #sec{
+        height: 450px;
+        margin-left: 30vh;
+    }
+    #text{
+        margin-top: 5vh;
+    }
+    #pro-text{
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 30px;
+        line-height: 35px;
+        color: #252E71;
+    }
+    #pr-text{
+        font-family: Roboto;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 18px;
+        line-height: 21px;
+        text-align: center;
+    }
+    #card-text{
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; 
+        font-style: normal;
+        font-weight: Bold;
+        font-size: 18px;
+        color: #252E71;
+    }
+    #counter{
+        background-color: #252E71;
+        height: 267px;
+        left: 0px;
+        top: 2046px;
+    }
+    #cc{
+        background-color:white;
+    }
+    #cc-text{
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 40px;
+        line-height: 47px;
+        color: #FCFCFC;
+    }
+    #cc-text-2{
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 14px;
+        line-height: 16px;
+        text-align: center;
+        color: #F8F8F8;
+    }
+    #pos1{
+        width: 211px;
+        height: 88px;
+        left: 180px;
+        margin-top: 6vh;
+    }
+    #pos2{
+        width: 182px;
+        height: 88.67px;
+        left: 90px;
+        margin-top: 6vh;
+    }
+    #pos3{
+        width: 182px;
+        height: 87.67px;
+        left: 0px;
+        margin-top: 6vh;
+    }
+    #pos4{
+        width: 129px;
+        height: 89px;
+        left: -100px;
+        margin-top: 6vh;
+    }
+    #pos5{
+        width: 157px;
+        height: 88px;
+        left: -240px;
+        margin-top: 6vh;
+    }
+    #portofolio{
+        height: 70vh;
+    }
+    #por-text{
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 30px;
+        line-height: 35px;
+        text-align: center;
+        color: #252E71;
+    }
+    #content-por{
+        margin-top:12vh;
+    }
+    #partnership{
+        height: 40vh;
+    }
+    #footer{
+        background-color: #252E71;
+        height: 343px;
+    }
+    #footer-text{
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 16px;
+        line-height: 19px;
+        color: #FFFFFF;
+    }
+    #footer-text2{
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 16px;
+        line-height: 19px;
+        color: #FFFFFF;
+    }
+    #copy{
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 18px;
+        line-height: 21px;
+        color: #FFFFFF;
+            }
+</style>
